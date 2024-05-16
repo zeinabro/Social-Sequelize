f57d648b82496c72943fed3ac7b2dabf76ab5ef4
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-16 14:21:06 +0100

    defined table associations

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const { Comment, Like, Post, Profile, User } = require("./models/index");
 
-// Define your associations here
+User.hasOne(Profile)
+Profile.belongsTo(User)
 
+User.hasMany(Post)
+Post.belongsTo(User)
 
+Post.hasMany(Comment)
+Comment.belongsTo(Post)
 
-
+User.belongsToMany(Like, {through:"UserLikes"})
+Like.belongsToMany(User, {through:"UserLikes"})
 
 module.exports = {
     Comment,
